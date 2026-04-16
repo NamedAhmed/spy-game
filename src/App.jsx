02ea0b57d58cb8a8b22a3861620e5f3bacda1113
NamedAhmed <Ahmed_Z101@hotmail.com>
2026-04-15 20:42:29 -0400

    first c2mit1

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,6 @@ function App() {
       )}
 
 
-
-
-
-
-{/*
-        <div style={{ backgroundColor: base_color, padding: '20px', borderRadius: '5px' }}>Item 1</div>
-        <div style={{ backgroundColor: base_color, padding: '20px', borderRadius: '5px' }}>Item 2</div>
-        <div style={{ backgroundColor: base_color, padding: '20px', borderRadius: '5px' }}>Item 3</div>
-        <div style={{ backgroundColor: base_color, padding: '20px', borderRadius: '5px' }}>Item 4</div>
-        <div style={{ backgroundColor: base_color, padding: '20px', borderRadius: '5px' }}>Item 5</div>
-        <div style={{ backgroundColor: base_color, padding: '20px', borderRadius: '5px' }}>Item 6</div>
-*/}
-
-
       </div>
     </div>
   );
